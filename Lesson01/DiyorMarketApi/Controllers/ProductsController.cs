89dd8bd2a91255d491fdee767bcffbc33220eaee
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2023-11-29 11:18:54 +0100

    inital project setup

---
+++ Lesson01/DiyorMarketApi/Controllers/ProductsController.cs
@@ -0,0 +1,55 @@
+﻿using DiyorMarketApi.Models;
+using DiyorMarketApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DiyorMarketApi.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // GET: api/<ProductsController>
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            return ProductsService.GetProducts();
+        }
+
+        // GET api/<ProductsController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Product> Get(int id)
+        {
+            var product = ProductsService.GetProduct(id);
+            
+            if (product is null)
+            {
+                return NotFound($"Product with id: {id} does not exist.");
+            }
+
+            return product;
+        }
+
+        // POST api/<ProductsController>
+        [HttpPost]
+        public void Post([FromBody] Product product)
+        {
+            ProductsService.Create(product);
+        }
+
+        // PUT api/<ProductsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Product productToUpdate)
+        {
+            ProductsService.Update(productToUpdate);
+        }
+
+        // DELETE api/<ProductsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            ProductsService.Delete(id);
+        }
+    }
+}
---
+++ Lesson01/DiyorMarketApi/Services/ProductsService.cs
@@ -0,0 +1,80 @@
+﻿using DiyorMarketApi.Models;
+using System.Linq;
+
+namespace DiyorMarketApi.Services
+{
+    public class ProductsService
+    {
+        private static List<Product> Products = new List<Product>
+        {
+            new Product
+            {
+                Id = 1,
+                Name = "Coca-Cola",
+                Price = 2500,
+                Category = "Drinks"
+            },
+            new Product
+            {
+                Id = 2,
+                Name = "Fanta",
+                Price = 2500,
+                Category = "Drinks"
+            },
+            new Product
+            {
+                Id = 3,
+                Name = "Sprite",
+                Price = 2450,
+                Category = "Drinks"
+            },
+            new Product
+            {
+                Id = 4,
+                Name = "Snikers",
+                Price = 3500,
+                Category = "Chocolate"
+            },
+            new Product
+            {
+                Id = 5,
+                Name = "Mars",
+                Price = 3200,
+                Category = "Chocolate"
+            },
+        };
+
+        public static List<Product> GetProducts()
+            => Products;
+
+        public static Product? GetProduct(int id)
+            => Products.FirstOrDefault(x => x.Id == id);
+
+        public static void Create(Product product)
+            => Products.Add(product);
+
+        public static void Update(Product product)
+        {
+            var productToUpdate = Products.FirstOrDefault(x => x.Id == product.Id);
+
+            if (productToUpdate is null)
+            {
+                return;
+            }
+
+            productToUpdate.Name = product.Name;
+            productToUpdate.Price = product.Price;
+            productToUpdate.Category = product.Category;
+        }
+
+        public static void Delete(int id)
+        {
+            var product = Products.FirstOrDefault(x => x.Id == id);
+
+            if (product is not null)
+            {
+                Products.Remove(product);
+            }
+        }
+    }
+}
