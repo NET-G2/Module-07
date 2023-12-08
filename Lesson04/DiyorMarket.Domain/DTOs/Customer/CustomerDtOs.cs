4d0774a312d87ab84e47adfbe382480ac789f4db
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2023-12-08 13:02:29 +0500

    added DTOs for Entites

---
+++ Lesson04/DiyorMarket.Domain/DTOs/Customer/CustomerDtOs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiyorMarket.Domain.DTOs.Customer
+{
+    public record CustomerDtOs(
+        int Id,
+        string Name,
+        int Age,
+        string PhoneNumber,
+        string Email
+        );
+    
+}
