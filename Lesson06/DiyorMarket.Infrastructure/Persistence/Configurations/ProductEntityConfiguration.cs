c30b71ef346f6754f11ab8375766c9934fa10e45
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2023-12-24 14:24:56 +0100

    update lesson 6

---
+++ Lesson06/DiyorMarket.Infrastructure/Persistence/Configurations/ProductEntityConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using DiyorMarket.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DiyorMarket.Infrastructure.Persistence.Configurations
+{
+    internal class ProductEntityConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable(nameof(Product));
+            builder.HasKey(p => p.Id);
+
+            builder.HasOne(p => p.Category)
+                .WithMany(c => c.Products)
+                .HasForeignKey(p => p.CategoryId);
+
+            builder.Property(p => p.Name)
+                .HasMaxLength(255)
+                .IsRequired();
+            builder.Property(p => p.Description)
+                .HasMaxLength(500);
+            builder.Property(p => p.Price)
+                .HasColumnType("money");
+        }
+    }
+}
---
+++ Lesson06/DiyorMarket.Infrastructure/Persistence/DiyorMarketDbContext.cs
@@ -0,0 +1,29 @@
+﻿using DiyorMarket.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiyorMarket.Infrastructure.Persistence
+{
+    public class DiyorMarketDbContext : DbContext
+    {
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+
+        public DiyorMarketDbContext(DbContextOptions<DiyorMarketDbContext> options)
+            : base(options)
+        {
+            Database.Migrate();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
